3467868c9f03ef8efa2baa1d127df7a075782de7
Grant Phelps <gbphelps2@gmail.com>
2019-06-05 18:45:28 -0400

    Add bumpDegree function

---
+++ javascripts/utils/bumpDegree.js
@@ -0,0 +1,27 @@
+//NOTE: takes an array of n control points and returns an array of n + 1 control points, 
+//so that the output array describes a bezier one degree higher than the input, but with
+//an identical curvature.
+
+export default function bumpDegree(points){
+    const result = [];
+    const n = points.length - 1;
+
+    function interpolate(a,b,i){
+        return i / (n + 1) * a + (n + 1 - i)/(n + 1) * b;
+    }
+
+
+    for (let i=0; i<=points.length; i++){
+        if (i === 0) { result.push(points[i]); continue; }
+        if (i === points.length) { result.push(points[i-1]); continue; }
+        const A = points[i-1];
+        const B = points[i];
+
+        result.push({
+            x: interpolate(A.x, B.x, i),
+            y: interpolate(A.y, B.y, i)
+        })
+    }
+
+    return result;
+}
